35f87f6a3f45b78268e263f5aacfa21b3b84c182
Olivier Bessuand <bessuand.o@gmail.com>
2021-03-16 15:28:58 +0100

    mise a jour

--- app/views/Front/accueil.php
+++ app/views/Front/accueil.php
@@ -1,12 +1,14 @@
 <?php ob_start(); ?>
 
 <h1>Bienvenue sur Blog-ludik!<br>le blog sur les jeux de société modernes </h1>
-<br>
+
+<div class="image-container">
+    <img src="app/public/Front/images/la-scene.jpg" alt="la scene">
+ </div> 
+
 <p class="text">Ce site a pour but de parler des jeux de société modernes,<br>
-Je donne mon avis perso sur les jeux présents dans ma ludothèque 
-</p>
-
-
+    Je donne mon avis perso sur les jeux présents dans ma ludothèque. 
+</p>  
 
  <?php $content = ob_get_clean(); ?><!--fonction PHP pour injecter le template  -->
 <?php require 'templates/template.php' ?>
